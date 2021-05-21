3147190b686c9fa275ed814437943ca2c78dc190
leomadhan <madhanmohanit@gmail.com>
2021-05-21 18:57:46 +0530

    Added 4 test in grid test

--- pages/gridPage.ts
+++ pages/gridPage.ts
@@ -26,4 +26,28 @@ export class GridPage {
   public static get columnHeader() {
     return $$('[aria-rowindex="1"] > [role="columnheader"]');
   }
+
+  public static get columnToolPanel() {
+    return $('#myGrid div.ag-side-bar > div:nth-child(2)');
+  }
+
+  public static get filerToolPanel() {
+    return $('#myGrid div.ag-side-bar > div:nth-child(3)');
+  }
+
+  public static get filterColumnArea(){
+    return $('//*[@id="myGrid"]//span[@ref="eFilterName"][text()="Name"]');
+  }
+
+  public static get filterText(){
+    return $('#filterText');
+  }
+
+  public static get girdFilterText(){
+    return $('input.ag-floating-filter-input');
+  }
+
+  public static get takeVideo(){
+    return $('div.video-tour > a');
+  }
 }
